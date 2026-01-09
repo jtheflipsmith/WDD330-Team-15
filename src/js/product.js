130b81b5a392dd30162b187ea96939551523fca2
christianuche <christianuche720@gmail.com>
2026-01-09 23:13:39 +0100

    Fixed the Add to Cart being broken

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,22 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs'; // import utility function to set local storage but it also suppoe to get it (fixed)
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  /* setLocalStorage('so-cart', product); This is incorrect, it overwrites the cart
+  */
+ let cartItems = getLocalStorage('so-cart') || []; // retrieve existing cart items or initialize as empty array
+
+ if (!Array.isArray(cartItems)) {
+    console.error('Cart data is corrupted. Resetting cart.');
+    cartItems = []; // This will basically reset to empty array if corrupted
+ }
+
+ cartItems.push(product); // add new product to cart array
+ setLocalStorage('so-cart', cartItems); // save updated cart back to local storage
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
