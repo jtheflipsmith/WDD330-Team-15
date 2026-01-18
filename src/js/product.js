88fb8ff8e80a793e46fa8a841dd7e90ece835127
Joshua Smith <joshuasmith@byui.edu>
2026-01-17 17:32:33 -0700

    ficing merge issues

--- src/js/product.js
+++ src/js/product.js
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-=======
 import { getLocalStorage, setLocalStorage } from './utils.mjs'; // import utility function to set local storage but it also suppoe to get it (fixed)
 import ProductData from './ProductData.mjs';
->>>>>>> f97687d8974b885763a2ec98b16f3938bc4a3886
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-<<<<<<< HEAD
   setLocalStorage("so-cart", product);
-=======
   /* setLocalStorage('so-cart', product); This is incorrect, it overwrites the cart
   */
  let cartItems = getLocalStorage('so-cart') || []; // retrieve existing cart items or initialize as empty array
@@ -23,7 +18,6 @@ function addProductToCart(product) {
 
  cartItems.push(product); // add new product to cart array
  setLocalStorage('so-cart', cartItems); // save updated cart back to local storage
->>>>>>> f97687d8974b885763a2ec98b16f3938bc4a3886
 }
 
 // add to cart button event handler
