ddbe37efee97d2da5697a602ab68c0f88068f32a
acponton <pon22008@byui.edu>
2026-01-17 19:39:44 -0600

    Week2_TeamActivity

--- src/js/product.js
+++ src/js/product.js
@@ -1,15 +1,14 @@
-import { getParam } from './utils.mjs'; // import utility function to set local storage but it also suppoe to get it (fixed)
-import ProductData from './ProductData.mjs';
+import { getParam } from "./utils.mjs"; // import utility function to set local storage but it also suppoe to get it (fixed)
+import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const productID = getParam('product');
-const dataSource = new ProductData('tents');
+const productID = getParam("product");
+const dataSource = new ProductData("tents");
 
 // console.log(dataSource.findProductById(productID));
 
 const product = new ProductDetails(productID, dataSource);
 product.init();
-
 
 // // add to cart button event handler
 // async function addToCartHandler(e) {
