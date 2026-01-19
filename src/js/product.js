173d0bab307d0976150996d0fa7cb584518d3b07
christianuche <christianuche720@gmail.com>
2026-01-19 04:58:14 +0100

    resolved-and-return-changes

--- src/js/product.js
+++ src/js/product.js
@@ -1,23 +1,18 @@
-import { getParam } from "./utils.mjs"; // import utility function to set local storage but it also suppoe to get it (fixed)
+import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
 
-const productID = getParam("product");
 const dataSource = new ProductData("tents");
-const productId = getParam("product");
 
-// console.log(dataSource.findProductById(productID));
+function addProductToCart(product) {
+  setLocalStorage("so-cart", product);
+}
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
 
-const product = new ProductDetails(productID, dataSource);
-product.init();
-
-// // add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await dataSource.findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
-
-// // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
